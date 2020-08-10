67d9c33129e0f3a429acb3f63cd86cd718d9092e
Katsushi OUGI <bdbkx286@gmail.com>
2020-08-10 18:20:08 +0900

    SSRでログイン

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 デモサイト https://nextjs-demo-lovat.vercel.app/
+
+
+Firebase sample
+
+https://github.com/vercel/next.js/tree/canary/examples/with-firebase-authentication
--- pages/ssr.js
+++ pages/ssr.js
@@ -1,19 +1,28 @@
-function Ssr({ posts }) {
+import { parseCookies } from 'nookies';
+
+function Ssr({ posts, user }) {
+
   return (
     <div>
       <p>サーバーサイドで動的にページを作ってHTMLを返す</p>
-      <ul>{posts.map((post) => <li>{post.title}</li>)}</ul>
+      <ul>{posts.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+      <p>Firebaseでログインした後のユーザー情報を取得してみる</p>
+      {user && <p>Email: {user.email}</p>}
     </div>
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
+  const cookie = parseCookies(ctx);
+  const user = cookie && cookie.auth ? JSON.parse(cookie.auth) : null
+
   const res = await fetch("https://api.github.com/repos/rails/rails/issues")
   const posts = await res.json();
 
   return {
     props: {
-      posts
+      posts,
+      user
     }
   }
 }
